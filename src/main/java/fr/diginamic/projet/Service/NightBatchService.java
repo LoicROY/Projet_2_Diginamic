d212c0828a1b4a682014bfde32cc0c7dcea00104
formation <formation@DESKTOP-SQJSFH8>
2021-07-20 12:27:47 +0200

    ajout switch nightbatch

--- src/main/java/fr/diginamic/projet/Service/NightBatchService.java
+++ src/main/java/fr/diginamic/projet/Service/NightBatchService.java
@@ -23,43 +23,65 @@ public class NightBatchService {
     public void traiterAbsenceChoisie() throws AbsenceException {
 
 
-       // List<Absence>absences =  repo.findAllByStatut(StatutType.INITIALE);
-        List<Absence>absences = (List<Absence>) repo.findAll();
-
+      List<Absence>absences =  repo.findAllByStatut(StatutType.INITIALE);
 
       for (Absence a : absences ){
 
           if (a instanceof AbsenceChoisie){
-
-              if (a instanceof CongeSansSolde){
-                  a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
-                  repo.save(a);
-              }
-              else if (a instanceof RttEmploye){
-                  if ((RttEmploye.NOMBRE_MAX - a.getSalarie().getSoldeRTT()) + DateUtils.interval(a) <= RttEmploye.NOMBRE_MAX){
-                      a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
-                      repo.save(a);
-                  }else{
-                      a.setStatut(StatutType.REJETEE);
-                      repo.save(a);
-                  }
-              }
-              else if (a instanceof CongePaye) {
-
-                  if ((CongePaye.NOMBRE_MAX - a.getSalarie().getSoldeCP()) + DateUtils.interval(a) <= CongePaye.NOMBRE_MAX){
-                      a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
-                      repo.save(a);
-                  }else{
-                      a.setStatut(StatutType.REJETEE);
-                      repo.save(a);
-                  }
-              }
+        	  String typeConge = a.getClass().getSimpleName();
+        	  switch(typeConge) {
+        	  	case "CongeSansSolde" :
+        	  		a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+                    repo.save(a);
+        	  		break;
+        	  	case "RttEmploye" :
+        	  		if ((RttEmploye.NOMBRE_MAX - a.getSalarie().getSoldeRTT()) + DateUtils.interval(a) <= RttEmploye.NOMBRE_MAX){
+                        a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+                        repo.save(a);
+                    }else{
+                        a.setStatut(StatutType.REJETEE);
+                        repo.save(a);
+                    }
+        	  		break;
+        	  	case "CongePaye" :
+        	  		if ((CongePaye.NOMBRE_MAX - a.getSalarie().getSoldeCP()) + DateUtils.interval(a) <= CongePaye.NOMBRE_MAX){
+                        a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+                        repo.save(a);
+                    }else{
+                        a.setStatut(StatutType.REJETEE);
+                        repo.save(a);
+                    }
+        	  		break;
+        	  		
+        	  }
+//              if (a instanceof CongeSansSolde){
+//                  a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+//                  repo.save(a);
+//              }
+//              else if (a instanceof RttEmploye){
+//                  if ((RttEmploye.NOMBRE_MAX - a.getSalarie().getSoldeRTT()) + DateUtils.interval(a) <= RttEmploye.NOMBRE_MAX){
+//                      a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+//                      repo.save(a);
+//                  }else{
+//                      a.setStatut(StatutType.REJETEE);
+//                      repo.save(a);
+//                  }
+//              }
+//              else if (a instanceof CongePaye) {
+//
+//                  if ((CongePaye.NOMBRE_MAX - a.getSalarie().getSoldeCP()) + DateUtils.interval(a) <= CongePaye.NOMBRE_MAX){
+//                      a.setStatut(StatutType.EN_ATTENTE_VALIDATION);
+//                      repo.save(a);
+//                  }else{
+//                      a.setStatut(StatutType.REJETEE);
+//                      repo.save(a);
+//                  }
+//              }
           }
       }
     }
 
     public void traiterAbsenceObligatoire(){
-
 
         List<Absence>absences=  repo.findAllByStatut(StatutType.INITIALE);
 
